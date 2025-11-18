96a8cb382ed42a7744939b2c29bfee91a7c15ca4
jjacque1 <jacksonjacque.jj@gmail.com>
2025-11-17 20:32:55 -0500

    created 3 .then/.catch/.finally for each function...fetchdProduct, fetchReviews and fetchSales

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -62,3 +62,43 @@ const fetchSalesReport = function (
 };
 
 
+//========IMPLEMENT ERROR HANDLING ON fetchProductCatalog===============
+
+fetchProductCatalog()
+.then(function (catalog) {
+    console.log("Product Catalog:", catalog);
+})
+.catch(function (error) {
+    console.log("Catalog Error:", error)
+})
+.finally(function () {
+    console.log("fetchProductCatalog has been attempted")
+})
+
+
+//========IMPLEMENT ERROR HANDLING ON fetchProductReviews===============
+
+fetchProductReviews(1)
+.then(function (reviews) {
+    console.log("Product Reviews:", reviews);
+})
+.catch(function (error) {
+    console.log("Reviews Error:", error)
+})
+.finally(function () {
+    console.log("fetchProductReviews has been attempted")
+})
+
+
+//========IMPLEMENT ERROR HANDLING ON fetchSalesReport===============
+
+fetchSalesReport(5)
+  .then(function (salesReport) {
+    console.log("Sales Report:", salesReport);
+  })
+  .catch(function (error) {
+    console.log("Sales Report Error:", error);
+  })
+  .finally(function () {
+    console.log("fetchSalesReport has been attempted.");
+  });
